b6047a2d8cf1159fbd2eb01d2ca90ab813a55885
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-03 23:19:53 +0800

    update 2-1 and 2-2

--- Chapter2/2-1.py
+++ Chapter2/2-1.py
@@ -1,2 +1,14 @@
+import sys
+
 mystring = 'Hello World!'
-print(mystring)
+print mystring
+
+print "%s is number %d!" % ("Python" , 1)
+
+print >> sys.stderr, 'Fatal Error: invalid input!'
+
+logfile = open('mylog.txt','a')
+
+print >> logfile, 'Fatal Error: invalid input!'
+
+logfile.close()
---
+++ Chapter2/2-2.py
@@ -0,0 +1,9 @@
+import sys
+user = raw_input("Enter login name: ")
+
+print 'Your login is :',user
+
+num = raw_input('Now enter a number:')
+
+print 'Doubling your numer : %d' % (int(num)*2)
+
