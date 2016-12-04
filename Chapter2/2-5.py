3a55b4a17a80844cd2057d37b663dc2eddaad153
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-04 22:15:17 +0800

    add 2 - 5

---
+++ Chapter2/2-5.py
@@ -0,0 +1,9 @@
+counter = 0
+miles = 1000.0
+name = 'Bob'
+counter = counter + 1
+kilometers = 1.609 * miles
+print '%f miles is the same as %f km' % (miles, kilometers)
+n = 10
+n = n * 10
+n *= 10
