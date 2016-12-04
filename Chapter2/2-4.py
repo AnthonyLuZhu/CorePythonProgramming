753fd4401a5779e405605e1665359575e6f6c9fc
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-04 22:03:04 +0800

    add 2 -4

---
+++ Chapter2/2-4.py
@@ -0,0 +1,21 @@
+print  -2 * 4 + 3 **2
+
+print 2 < 4
+
+print 2 == 4
+
+print  2 > 4
+
+print 6.2 <= 6
+
+print  6.2 <= 6.2
+
+print  6.2 <= 6.20001
+
+print  2 < 4 and  2 == 4
+
+print 2 > 4  or 2 < 4
+
+print not 6.2  <= 6
+
+print  3 < 4 < 5
