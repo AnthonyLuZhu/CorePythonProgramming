384c1a03a471fa58fa312fdd5f669dd6cbb901b0
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-04 22:32:37 +0800

    add 2 - 7

---
+++ Chapter2/2-7.py
@@ -0,0 +1,25 @@
+pystr = 'Python'
+iscool = 'is cool!'
+print pystr[0]
+
+print  pystr [2:5]
+
+print iscool[:2]
+
+print iscool[3:]
+
+print iscool[-1]
+
+print pystr + iscool
+
+print pystr + ' ' + iscool
+
+print pystr * 2
+
+print '-'* 20
+
+pystr = '''python ... is cool'''
+
+print pystr
+
+print 'python \n is cool'
