138178ad4fb1e8c1ab2a38d9ddea717a1637e1d1
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-07 13:02:14 +0800

    add 3-1,3-2

---
+++ Chapter3/3-1.py
@@ -0,0 +1,15 @@
+# check conditions
+weather_is_not = 0
+shark_warnings = 0
+if(weather_is_not  == 1) and \
+        (shark_warnings == 0):
+     print 'abc'
+
+print '''hi there, this is a long mesage for you
+that goes over multiple lines ...you will find
+out soon that triple quotes in Python allow
+this kind of fun! it is like a day on the beach'''
+
+go_surf, get_a_tan_while, boat_size , toll_money = (1,'windsurfing', 40.0, -2.00)
+
+import sys;  x = 'foo'; sys.stdout.write(x + '\n')
---
+++ Chapter3/3-2.py
@@ -0,0 +1,39 @@
+anInt = -12
+aString = 'cart'
+aFloat = -3.1415 * (5.0 ** 2)
+anoterString  = 'shop' + 'ping'
+aList = [3.14e10, '2nd elmt of a list', 8.82-4.371j]
+
+x = 1
+y = x= x + 1
+
+print x, y
+
+x += 1
+print x
+
+
+m = 12
+m%=5
+print m
+
+m**=2
+print m
+
+aList = [123, 'xyz']
+aList += [45.6e7]
+
+print aList
+
+x = y = z = 1
+print x, y ,z
+
+(x,y,z) = (1,2,'a string')
+print x,y,z
+
+x,y = 1,2
+print x,y
+
+x,y = y, x
+
+print x, y
