0c4b9ec1e3c58a0e89a1d376cb8b6dbc09139f86
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-13 22:26:30 +0800

    add 4-3 and 4-5

---
+++ Chapter4/4-5.py
@@ -0,0 +1,50 @@
+print 2==2
+
+print 2.46 <= 8.33
+
+#print (5+4j) >= (2-3j)
+
+print 'abc' == 'xyz'
+
+print 'abc' > 'xyz'
+
+print 'abc' < 'xyz'
+
+print [3,'abc'] == ['abc', 3]
+
+print [3, 'abc'] == [3,'abc']
+
+print 3 < 4 < 7
+
+print  4 > 3  == 3
+
+print  4 < 3 < 5 !=2 < 7
+
+a = [5 , 'hat', -9.3]
+
+b = a
+
+print a is b
+
+b = 3
+
+print b
+
+
+print a is b
+
+print a is not b
+
+
+
+x, y = 3.14 , -1024
+
+print  x  < 5.0
+
+print not ( x < 5.0)
+
+print ( x < 5.0) or (y > 2.71)
+
+print (x < 5.0) and (y > 2.71)
+
+print not (x is y)
