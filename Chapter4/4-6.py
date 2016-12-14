da8c9b5562c3e3c334444b073267680f568caa03
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-14 22:57:02 +0800

    add 4-6

---
+++ Chapter4/4-6.py
@@ -0,0 +1,28 @@
+print type(4)
+
+print  type('Hello World')
+
+print type(type(4))
+
+a,b = -4, 12
+print cmp(a,b)
+
+b = -4
+print cmp(a,b)
+
+a,b = 'abc','xyz'
+print cmp(a,b)
+
+b = 'abc'
+print cmp(a,b)
+
+print str(1)
+
+print (2e10)
+
+print str([0,5,9,9])
+
+print repr([0,5,9,9])
+
+
+
