03391cbd27ba16216909fc3caa65d38eded6083f
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-19 20:06:56 +0800

    update 5-5 and 5-6

---
+++ Chapter5/5-3.py
@@ -0,0 +1,35 @@
+#from  __future__ import division
+print -442 - 77
+
+print 4 ** 3
+
+print  4.2 ** 3.2
+
+print  8 / 3
+
+print 8.0 / 3.0
+
+print  8 % 3
+
+print (60. - 32. ) * (5./ 9.)
+
+print 14 * 0x04
+
+print 0170 / 4
+
+print 0x80  + 0777
+
+print 45L *  22L
+
+print 16399L + 0xA94E8L
+
+print  -2147483648L - 52147483648L
+
+print 64.375 + 1j + 4.23 + 8.5j
+
+print  0 + 1j  ** 2
+
+print  1 + 1j  ** 2
+
+print (1 + 1j) ** 2
+
---
+++ Chapter5/5-5.py
@@ -0,0 +1,17 @@
+print 30 & 45
+
+print 30 | 45
+
+print 45 & 60
+
+print 45 | 60
+
+print ~30
+
+print ~45
+
+print 45 << 1
+
+print 60 >> 2
+
+print 30 ^ 45
