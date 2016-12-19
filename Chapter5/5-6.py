8b592a189e7ac4cc9a5dd01e06abfb1f593de1c5
Anthony Zhu <anthonyluzhu@gmail.com>
2016-12-19 20:28:33 +0800

    update 5-5 and 5-6

---
+++ Chapter5/5-6.py
@@ -0,0 +1,48 @@
+print  cmp(-6,2)
+
+print cmp(-4.33333, -2.718281828)
+
+print cmp(0xFF, 255)
+
+print str(0xFF)
+
+print str(55.3e2)
+
+print type(0xFF)
+
+print type(98765432109876543210L)
+
+print type(2-1j)
+
+print int(4.25555)
+
+print long (42)
+
+print float(4)
+
+print complex(4)
+
+print complex (2.4, -8)
+
+print complex (2.3e-10, 45.3e4)
+
+print abs(-1)
+
+print (10.0)
+
+print abs(1.2 - 2.1j)
+
+print abs(0.23 - 0.78)
+
+print coerce(1,2)
+
+print (1.3, 134L)
+
+print (1,134L)
+
+print (1j , 134L)
+
+
+
+
+
