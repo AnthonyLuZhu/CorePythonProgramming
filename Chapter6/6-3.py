96948a3b5f9a01c31cdbe96fb17f0c1faf335cf9
Anthony Zhu <anthonyluzhu@gmail.com>
2017-01-06 22:43:19 +0800

    6-1 and 6-2

---
+++ Chapter6/6-3.py
@@ -0,0 +1,28 @@
+str1 = 'abc'
+str2 = 'lmn'
+str3 = 'xyz'
+print (str1 < str2)
+
+print (str2 != str3)
+
+print (str1 < str3 and str2 == 'xyz')
+
+aString = 'abcd'
+print len(aString)
+
+print aString[0]
+
+print aString[1:3]
+
+print aString[2:4]
+
+print aString[-1]
+
+print aString[-3: -1]
+
+print aString[-4]
+
+print ('bc' in 'abcd')
+print 'n' in 'abcd'
+print 'nm' not in 'abcd'
+
