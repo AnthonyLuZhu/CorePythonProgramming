a13478de8f36ed2bb9399287678511da691c8a16
Anthony Zhu <anthonyluzhu@gmail.com>
2017-02-07 22:37:17 +0800

    6-6

---
+++ Chapter6/6-6.py
@@ -0,0 +1,22 @@
+quest = 'what is your favorite color'
+print quest.capitalize()
+
+print quest.center(40)
+
+print quest.count('or')
+
+print quest.endswith('blue')
+
+print quest.endswith('color?')
+
+print quest.find('or', 30)
+
+print quest.find('or',22)
+
+print quest.index('or',10)
+
+print ':'.join(quest.split())
+
+print quest.replace('favorite color', 'quest')
+
+print quest.upper()
