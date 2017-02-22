79d0265c52f802b9dc2c891c83617911a10b99a5
Anthony Zhu <anthonyluzhu@gmail.com>
2017-02-22 22:38:33 +0800

    6-11

---
+++ Chapter6/6-11.py
@@ -0,0 +1,44 @@
+aList = [123,'abc',4.56,['inner', 'list'], 7-9j]
+antherList = [None, 'something to see here']
+print aList
+
+print antherList
+
+aListThatStartedEmpty = []
+print aListThatStartedEmpty
+
+print list('foo')
+
+print aList[0]
+
+print aList[1:4]
+
+print aList[:3]
+
+print aList[3][1]
+
+print aList
+
+print aList[2]
+
+aList[2] = 'float replacer'
+
+print aList
+
+antherList.append("hi, i'm new here")
+
+print antherList
+
+aListThatStartedEmpty.append("not empty anymore")
+
+print aListThatStartedEmpty
+
+print aList
+
+del aList[1]
+
+print aList
+
+aList.remove(123)
+
+print aList
